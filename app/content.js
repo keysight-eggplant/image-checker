fb11a58ff7b6097a62e4a87360364dbb86340486
Liviu Taban <tabanliviu@yahoo.com>
2017-04-13 17:34:30 +0100

    [SQUAD-11][SQUAD-36][REFACTOR] more refactoring/renaming

--- app/content.js
+++ app/content.js
@@ -1,11 +1,11 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'start') {
-    let domNodes = window.NCC.imageChecker._collectionToArray(document.getElementsByTagName('*'));
+    let domNodes = window.NCC.imageChecker._nodeListToArray(document.getElementsByTagName('*'));
     window.NCC.imageChecker.showImagesInfo(domNodes);
   }
 
   if (request.message === 'stop') {
-    window.NCC.imageChecker._collectionToArray(document.querySelectorAll('.ncc-image-checker-overlay')).map(o => o.remove());
+    window.NCC.imageChecker._nodeListToArray(document.querySelectorAll('.ncc-image-checker-overlay')).map(o => o.remove());
   }
 
   if (request.message === 'info') {
