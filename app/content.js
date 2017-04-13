f2a74cc65f22e98aa605915ffe43eac7423ab4e4
Liviu Taban <tabanliviu@yahoo.com>
2017-04-13 16:15:52 +0100

    [SQUAD-11][SQUAD-36][REFACTOR] ensure popup actions are available after the content script has been loaded and fix minor url parsing issues

--- app/content.js
+++ app/content.js
@@ -1,17 +1,18 @@
-/*global chrome*/
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "start") {
+  if (request.message === 'start') {
     let domNodes = window.NCC.imageChecker._collectionToArray(document.getElementsByTagName('*'));
     window.NCC.imageChecker.showImagesInfo(domNodes);
   }
 
-  if (request.message === "stop") {
+  if (request.message === 'stop') {
     window.NCC.imageChecker._collectionToArray(document.querySelectorAll('.ncc-image-checker-overlay')).map(o => o.remove());
   }
 
-  if (request.message === "info") {
+  if (request.message === 'info') {
     document.querySelectorAll('.ncc-image-checker-overlay').length > 0
-      ? sendResponse({message: "active"})
-      : sendResponse({message: "inactive"});
+      ? sendResponse({message: 'active'})
+      : sendResponse({message: 'inactive'});
   }
 });
+
+chrome.runtime.sendMessage({message: 'loaded'});
