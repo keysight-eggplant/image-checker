36e01511d2138c5d37efade0b09fec475ab17a53
Liviu <tabanliviu@yahoo.com>
2017-04-21 17:26:14 +0100

    Revert "Tidying up the popup and getting file sizes."

--- app/public.js
+++ app/public.js
@@ -44,9 +44,9 @@
         }
       } else {
         let info = `Coverage: ${ getImageCoverage(image).toFixed(2) }%`;
-        var entry = performance.getEntriesByName(image.url)[0];
-        if (entry && entry.transferSize) {
-            info += `, File Size: ${ entry.transferSize/1000 } KB`;
+
+        if (image.size && image.size > 0) {
+          info += `, File Size: ${ image.size } KB`;
         }
 
         info += `, URL: ${ image.url }`;
@@ -86,13 +86,11 @@
     optimalP.innerHTML = `Image coverage: ${ getImageCoverage(image).toFixed(2) }%`;
     div.appendChild(optimalP);
 
-		var entry = performance.getEntriesByName(image.url)[0];
-    
-    if (entry && entry.transferSize) {
-		  let sizeP = document.createElement('p');
-      sizeP.innerHTML = `File Size: ${ entry.transferSize/1000 } KB`;
+    if (image.size) {
+      let sizeP = document.createElement('p');
+      sizeP.innerHTML = image.size > 0 ? `File Size: ${ image.size } KB` : `File size unavailable`;
       div.appendChild(sizeP);
-		}
+    }
   }
 
   function appendAnchorToBody(element, image, info) {
